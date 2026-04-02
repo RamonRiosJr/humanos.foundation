2d4cb6e63d03ca774ab7757ef3b04107f4f7af2a
RamonRiosJr <RamonRiosJr@users.noreply.github.com>
2026-04-02 12:04:58 +0000

    deploy: bb89bbc18b50a42390bb039f59ccf0d6d9aefbe0

---
+++ assets/file-x-DRekxO7J.js
@@ -0,0 +1,11 @@
+import{a as e}from"./index-BwAblSnu.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]],t=e("Clock",o);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"m14.5 12.5-5 5",key:"b62r18"}],["path",{d:"m9.5 12.5 5 5",key:"1rk7el"}]],l=e("FileX",c);export{t as C,l as F};
